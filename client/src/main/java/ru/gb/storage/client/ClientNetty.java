df4d1594d8795b73969ce93e2dcb9367098736aa
LidiaEvmenenko <inu-chan@mail.ru>
2022-04-24 22:11:16 +0500

    Initial commit

---
+++ client/src/main/java/ru/gb/storage/client/ClientNetty.java
@@ -0,0 +1,53 @@
+package ru.gb.storage.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import ru.gb.storage.commons.handler.JsonDecoder;
+import ru.gb.storage.commons.handler.JsonEncoder;
+
+public class ClientNetty {
+
+    public static void main(String[] args) throws InterruptedException {
+        new ClientNetty().start();
+    }
+
+    public void start() throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup(1);
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(
+                                    new LengthFieldBasedFrameDecoder(1024*1024,0,3,0,3),
+                                    new LengthFieldPrepender(3),
+                                    new JsonDecoder(),
+                                    new JsonEncoder(),
+                                    new ChannelInboundHandlerAdapter(){
+
+                                        @Override
+                                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                            super.channelRead(ctx, msg);
+                                        }
+                                    }
+
+                            );
+                        }
+                    });
+
+            ChannelFuture future = bootstrap.connect("localhost", 9000).sync();
+            System.out.println("Client started.");
+            Channel channel = future.channel();
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ server/src/main/java/ru/gb/storage/server/Server.java
@@ -0,0 +1,65 @@
+package ru.gb.storage.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import ru.gb.storage.commons.handler.JsonDecoder;
+import ru.gb.storage.commons.handler.JsonEncoder;
+
+public class Server implements Runnable{
+    private int port;
+
+    public static void main(String[] args) {
+        new Server(9000);
+    }
+
+    public Server(int port) {
+        this.port = port;
+        run();
+    }
+
+    @Override
+    public void run() {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(
+                                    new LengthFieldBasedFrameDecoder(1024*1024,0,3,0,3),
+                                    new LengthFieldPrepender(3),
+                                    new JsonDecoder(),
+                                    new JsonEncoder(),
+                                    new ChannelInboundHandlerAdapter(){
+                                        @Override
+                                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+                                        }
+
+                                    });//ServerInputHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    // ограничение очереди заявок на обработку(применяется только для сервера)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            //применяется для всех, говорит,что будет поддерживать соединение с клиентом
+            ChannelFuture future = server.bind(port).sync();// запускаем сервер
+            System.out.println("Server started.");
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+    }
+}
